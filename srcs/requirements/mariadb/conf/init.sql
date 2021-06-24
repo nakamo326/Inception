566db5408b39d55e1304e2fec01b4912b6ed8d69
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-06-24 14:51:06 +0900

    fix wordpress

--- srcs/requirements/mariadb/conf/init.sql
+++
@@ -1,3 +0,0 @@
-create user 'wordpress_user'@'wordpress' identified by 'wordpress_pass';
-create database wordpressdb;
-grant all privileges on wordpressdb.* to 'wordpress_user'@'wordpress' with grant option;
--- srcs/requirements/wordpress/init.sh
+++ srcs/requirements/wordpress/init.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#! /bin/sh
+#makeでやるべき？
 
 curl https://ja.wordpress.org/latest-ja.zip -o wp_site.zip
 unzip wp_site.zip
-mv wordpress wp_site
+cp -r wordpress/* html
 rm wp_site.zip
 cp ./conf/wp-config.php wp_site/wp-config.php
