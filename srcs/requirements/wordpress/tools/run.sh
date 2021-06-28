fcc1587f4d732aaa013afcf911a290745115d62c
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-06-28 10:33:22 +0900

    fix wp init

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -10,13 +10,12 @@ if [ $? -ne 0 ]; then
     --admin_user=$ADMIN_USER \
     --admin_password=$ADMIN_PASS \
     --admin_email=$ADMIN_MAIL
+    wp theme activate twentytwentyone
 fi
 
 wp user get $AUTHOR_USER
 if [ $? -ne 0 ]; then
     wp user create $AUTHOR_USER $AUTHOR_MAIL --user_pass=$AUTHOR_PASS --role=author
 fi
-
-wp theme activate twentytwentyone
 
 exec php-fpm7 -F -R
